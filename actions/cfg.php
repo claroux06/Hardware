1e3e2f010d580ea6f5c190dd0a064c14298dc9a0
claroux06 <117074518+claroux06@users.noreply.github.com>
2023-11-24 16:14:07 +0100

    Update cfg.php

--- actions/cfg.php
+++ actions/cfg.php
@@ -4,15 +4,9 @@
 session_start();
 
 $pdo = new PDO(
-<<<<<<< HEAD
     'mysql:host=localhost;dbname=ESP32-S3;',
     'root',
     'root',
-=======
-    'mysql:host=localhost;dbname=esp32;',
-    'root',
-    '1234',
->>>>>>> origin/main
     array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
